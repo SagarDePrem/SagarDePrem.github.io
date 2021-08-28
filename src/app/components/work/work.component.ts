1ad842b4251a42f1dc38b89e860d3194b2128768
Prem <sagardeprem@gmail.com>
2021-08-29 00:40:48 +0530

    Fix worktab isseus

--- src/app/components/work/work.component.ts
+++ src/app/components/work/work.component.ts
@@ -18,26 +18,41 @@ export class WorkComponent implements OnInit {
   constructor() { 
   }
   workNodes: any= [];
-  workItems: any=[]
+  workItems: any=[];
+  workProjects: any = [];
   activeTab: any = [];
-  activeWorkItems: any = [];
+  activeWorkItems: any = {};
 
   ngOnInit(): void {
     this.workNodes = workNodes;
     this.workItems = workItems;
-    this.activeTab = workNodes[0].name;
-    this.setActiveTab(this.activeTab)
-  }
 
-  setActiveTab(val: string){
-    console.log(val)
-    this.activeTab = val;
-    this.activeWorkItems = [];
-    for (let i = 0; i < workItems.length; i++) { 
-      if (workItems[i].tags.includes(this.activeTab)){
-        this.activeWorkItems.push(workItems[i])
+    for (let i=0; i < workNodes.length; i++){
+      this.workProjects[workNodes[i].name] = []
+    }
+    console.log(workNodes)
+    for (let i=0; i< workItems.length; i++){
+      for (let j=0; j<workItems[i].tags.length; j++){
+        this.workProjects[workItems[i].tags[j]].push(workItems[i])
       }
     }
-    // console.log(this.activeWorkItems)
+    console.log(this.workProjects)
+
+    // this.activeTab = workNodes[0].name;
+    // this.setActiveTab(this.activeTab)
   }
+
+  // setActiveTab(val: string){
+  //   console.log(val)
+  //   this.activeTab = val;
+  //   this.activeWorkItems = {};
+
+
+  //   for (let i = 0; i < workItems.length; i++) { 
+  //     if (workItems[i].tags.includes(this.activeTab)){
+  //       this.activeWorkItems.push(workItems[i])
+  //     }
+  //   }
+  //   console.log(this.activeWorkItems)
+  // }
 }
